8403a27d0d3d50232d672d3bb894e3c5c6b031b5
Suraj-Dal <dsurajdal10@gmail.com>
2022-07-04 12:16:28 +0530

    [Suraj]: Usecase 5 to calculate wages till condition of total working hrs of 160 or working days 20 is reached.

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,30 +1,31 @@
-const IS_ABSENT = 0;
-const WAGE_PER_HRS = 20;
-const totalWorkingDays = 20;
+const IS_ABSENT = 0, WAGE_PER_HRS = 20, TOTAL_WORKING_DAYS = 20, MAX_WORKING_HRS = 160;
 let empCheck = Math.floor(Math.random() * 10) % 2;
-    //UC2 to calculate Daily Wages of employee.
-    let empHrs = 0;
-    //UC3 to create function to get Working HRS.
-    function getWorkingHrs(checkHrs)
+let empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+
+//UC3 to create function to get Working HRS.
+function getWorkingHrs(checkHrs)
+{
+    switch (checkHrs)
     {
-        switch (checkHrs)
-        {
-            case 1:
-                return 8;
-            case 2:
-                return 4;
-            default:
-                return 0;
-        }
+        case 1:
+            return 8;
+        case 2:
+            return 4;
+        default:
+            return 0;
     }
-    //UC4 calculate Emp Wage for month
-    let totalEmpHrs = 0;
-    console.log("----------Employee Wage Calculator----------");
-    for (let day = 0; day < totalWorkingDays; day++)
-    {
-        let checkHrs = (Math.floor(Math.random() * 10) % 3) + 1;
-        empHrs = getWorkingHrs(checkHrs);
-        totalEmpHrs += empHrs;
-    }
-    let totalWage = totalEmpHrs * WAGE_PER_HRS;
-    console.log("Total Working Hrs: "+ totalEmpHrs +" \nWages of employee for day is: " + totalWage);
+}
+
+//UC4 calculate Emp Wage for month
+console.log("----------Employee Wage Calculator----------");
+while (totalEmpHrs <= MAX_WORKING_HRS && totalWorkingDays < TOTAL_WORKING_DAYS) 
+{
+    // UC5 calculate Emp wage till condition of total working Hrs of 160 or max days of 20
+    let checkHrs = (Math.floor(Math.random() * 10) % 3) + 1;
+    empHrs = getWorkingHrs(checkHrs);
+    totalEmpHrs += empHrs;
+    totalWorkingDays++;
+}
+
+let totalWage = totalEmpHrs * WAGE_PER_HRS;
+console.log("Total Working Days: "+ totalWorkingDays +"\nTotal Working Hrs: "+ totalEmpHrs +" \nWages of employee for day is: " + totalWage);
