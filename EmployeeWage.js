11a6009f310397319c3043a2000e99275037bb00
Suraj-Dal <dsurajdal10@gmail.com>
2022-07-05 11:49:09 +0530

    [Suraj]: Usecase 7 to perform operations using array helper function is added.

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -2,6 +2,7 @@ const IS_ABSENT = 0, WAGE_PER_HRS = 20, TOTAL_WORKING_DAYS = 20, MAX_WORKING_HRS
 let empCheck = Math.floor(Math.random() * 10) % 2;
 let empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 let dailyWagesArr = new Array();
+
 //UC3 to create function to get Working HRS.
 function getWorkingHrs(checkHrs)
 {
@@ -15,6 +16,7 @@ function getWorkingHrs(checkHrs)
             return 0;
     }
 }
+
 function dailyWage(empHrs)
 {
     return empHrs * WAGE_PER_HRS;
@@ -27,14 +29,65 @@ while (totalEmpHrs <= MAX_WORKING_HRS && totalWorkingDays < TOTAL_WORKING_DAYS)
     // UC5 calculate Emp wage till condition of total working Hrs of 160 or max days of 20
     let checkHrs = (Math.floor(Math.random() * 10) % 3) + 1;
     empHrs = getWorkingHrs(checkHrs);
-    dailyWagesArr.push(dailyWage(empHrs));//UC 6 to store daily wage along with total wage.
+    dailyWagesArr.push(dailyWage(empHrs));
     totalEmpHrs += empHrs;
     totalWorkingDays++;
 }
+
+//UC 6 to store daily wage along with total wage.
 let day = 1;
+let dayWithWageDic = new Map;
 dailyWagesArr.forEach(element => {
     console.log("Day "+ day +" Wage: "+ element);
+    dayWithWageDic.set(day, element);
     day++;
 });
 let totalWage = dailyWage(totalEmpHrs);
-console.log("Total Working Days: "+ totalWorkingDays +"\nTotal Working Hrs: "+ totalEmpHrs +" \nWages of employee for day is: " + totalWage);
+console.log("Total Working Days: "+ totalWorkingDays +"\nTotal Working Hrs: "+ totalEmpHrs +" \nTotal Monthly Wage : " + totalWage);
+
+//UC7A - total wage using daily wage.
+let wageForDay = dailyWage(empHrs);
+let monthlyWage = 0;
+function sum(wageForDay)
+{
+    monthlyWage += wageForDay;
+}
+dailyWagesArr.forEach(sum);
+console.log("Total monthly wage using foreach method: "+ monthlyWage);
+
+//UC7C - Day when full day wage of 160 is earned.
+function fullDayWage(wageForDay)
+{
+    console.log("Days When full day wage of 160 is earned:");
+    dayWithWageDic.forEach((value, key) => {
+        if (value == "160")
+        console.log("Day "+ key +" Wage "+ value);
+    })
+}
+fullDayWage(wageForDay);
+
+//UC7D - first occurrence of full day wage.
+for (var [key, value] of dayWithWageDic)
+{
+    if (value == "160")
+    {
+        var first = key;
+        break;
+    }
+}
+console.log("First occurrence of full day wage is at Day "+ first);
+
+//UC7F part time wages
+console.log("Days When part time wage of 80 is earned:");
+dayWithWageDic.forEach((value, key) => {
+    if (value == "80")
+        console.log("Day "+ key +" Wage "+ value);
+})
+
+//UC7G no. of days employee worked.
+let presentDays = 0;
+dayWithWageDic.forEach((value, key) => {
+    if (value != "0")
+        presentDays++;
+})
+console.log("Employee worked for "+ presentDays +" Days.");
